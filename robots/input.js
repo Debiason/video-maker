7587550100b57f52762bbe31688b35a3e4f83dfc
Douglas Biason <douglas.biason@rbmweb.com.br>
2020-09-08 21:15:24 -0300

    feat: create video robot

--- robots/input.js
+++ robots/input.js
@@ -1,25 +1,29 @@
-const readline = require('readline-sync');
-const state = require('./state.js');
+const readline = require('readline-sync')
+const state = require('./state.js')
 
-function robot(){
-    const content = {
-        maximumSentences: 7
-    }
-    
-    content.searchTerm = asKAndReturnSearchTerm()
-    content.prefix = asKAndReturnPrefix()
-    state.save(content)
-    
-    function asKAndReturnSearchTerm() {
-        return readline.question('Digite um termo de pesquisa da Wikipedia:')
-    }
-    
-    function asKAndReturnPrefix(){
-        const prefixes = ['Quem é', 'o que é', 'A história de']
-        const selectedPrefixIndex = readline.keyInSelect(prefixes,'Escolha uma opção:')
-        const selectedPrefixText = prefixes[selectedPrefixIndex]
-        return selectedPrefixText
-    }
+function robot() {
+  const content = {
+    maximumSentences: 7
+  }
+
+  content.searchTerm = askAndReturnSearchTerm()
+  content.prefix = askAndReturnPrefix()
+  state.save(content)
+
+  function askAndReturnSearchTerm() {
+    return readline.question('Type a Wikipedia search term: ')
+  }
+
+  function askAndReturnPrefix() {
+    const prefixes = ['Who is', 'What is', 'The history of']
+    const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+    const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+    return selectedPrefixText
+  }
+
 }
+
+module.exports = robot
 
 module.exports = robot
